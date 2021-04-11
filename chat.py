4f8bb2b2f3bd8f4fbf3e0820b85cfd612c0ae03e
Pratyusha R <pratyusharavishankar@gmail.com>
2021-04-11 18:47:58 +0530

    Update chat.py
    New update with Bi LSTM

--- chat.py
+++ chat.py
@@ -1,12 +1,19 @@
 import random
 import json
 import torch
-from model import NeuralNet
+
+# import mysql.connector
+# import streamlit as st
+from asd import NeuralNet
 from utils import bag_of_word,preprocesss_text
 import nltk
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
-device = 'cuda'
+device = 'cpu'
+
+# mydb = mysql.connector.connect(host = 'localhost', user = 'root', passwd = 'EkMzopLL1928!$')
+
+# st.title('DSC Chatbot')
 
 with open ('data.json','r') as f:
     chats = json.load(f)
@@ -14,30 +21,31 @@ with open ('data.json','r') as f:
 FILE = "data.pth"
 data = torch.load(FILE)
 
-input_size = data['input_size']
+embed_dim = data['embed_dim']
 output_size = data['output_size']
 hidden_dim = data['hidden_dim']
 all_words = data['all_words']
 tags = data['tags']
 model_state = data['model_state']
 
-model = NeuralNet(input_size,hidden_dim,output_size).to(device)
+model = NeuralNet(embed_dim,hidden_dim,output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
-
-bot_name = 'sam'
+# st.markdown("Hi DSC Bot here")
+bot_name = 'DSC Bot'
 print("Yo chatter box || 'quit' to exit")
 
+
 while True:
-    sentence = input('You: ')
+    sentence = input('You : ')
     if sentence == 'quit':
         break
     else:
-        sentence = nltk.wordpunct_tokenize(sentence)
-        sentence = [stemmer.stem(word.lower()) for word in sentence]
+        sentence = preprocesss_text(sentence)
+
         X = bag_of_word(sentence,all_words)
         X = X.reshape(1,X.shape[0])
-        X = torch.from_numpy(X).float().to(device)
+        X = torch.from_numpy(X).to(torch.int64).to(device)
 
         output = model(X)
         _,predicted = torch.max(output,dim=1)
@@ -45,7 +53,7 @@ while True:
 
         probs = torch.softmax(output,dim=1)
         prob = probs[0][predicted.item()]
-        print(prob)
+        # print(prob)
         if prob > 0.5:
             for intent in chats['intents']:
                 if tag == intent["tag"]:
