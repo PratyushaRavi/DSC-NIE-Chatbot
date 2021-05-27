0193ee1aff2e30f381636aa499a321343a8f0c40
Pratyusha R <pratyusharavishankar@gmail.com>
2021-05-27 21:43:34 +0530

    updated hyperparameters

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
 import torch
 import torch.nn as nn
-total_words = 100
-embed_dim = 7
+
 class NeuralNet(nn.Module):
-    def __init__(self,embed_dim,hidden_size,no_class):
+    def __init__(self,total_words,embed_dim,hidden_size,no_class):
         super(NeuralNet, self).__init__()
         self.embed = nn.Embedding(total_words, embed_dim, padding_idx=0)
         self.lstm = nn.LSTM(input_size = embed_dim, hidden_size =hidden_size,num_layers = 1, batch_first =True, bidirectional=True)
@@ -19,3 +18,14 @@ class NeuralNet(nn.Module):
         out = self.relu(out)
         out = self.l2(out)
         return out
+
+# inp = torch.tensor([[1,2, 12,34, 56,78, 90,80],
+#                  [12,45, 99,67, 6,23, 77,82],
+#                  [11, 45, 99, 67, 6, 23, 77, 82],
+#                  [3,24, 6,99, 12,56, 21,22]])
+#
+#
+# m = NeuralNet(32,11,3)
+# print(m(inp))
+
+
